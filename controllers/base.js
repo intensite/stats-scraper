79ad534ccb695436bdd389bbf78944f139d1845d
intensite <stephen@intensite.ca>
2016-10-17 07:11:11 -0400

    First commit

---
+++ controllers/base.js
@@ -0,0 +1,10 @@
+var _ = require("lodash");
+module.exports = {
+    name: "base",
+    extend: function(child) {
+        return _.extend({}, this, child);
+    },
+    run: function(req, res, next) {
+
+    }
+}
